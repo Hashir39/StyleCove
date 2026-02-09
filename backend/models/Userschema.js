8d94cea6dea716cda31bd8f25ab981200ada6f22
Muhammad Hashir <126954940+Hashir39@users.noreply.github.com>
2026-02-09 17:52:43 +0500

    Add required and unique constraints to username field

--- backend/models/Userschema.js
+++ backend/models/Userschema.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const Userschema = new mongoose.Schema({
     username:{
         type: String,
-       
+        required:true,
+        unique:true 
     },
     email:{
         type: String,
