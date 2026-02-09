cd118e38b1afced990a936bbbe6be1d2cb908190
Muhammad Hashir <126954940+Hashir39@users.noreply.github.com>
2026-02-09 17:52:30 +0500

    Update Userschema.js

--- backend/models/Userschema.js
+++ backend/models/Userschema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Userschema = new mongoose.Schema({
     username:{
         type: String,
-        required:true
+       
     },
     email:{
         type: String,
